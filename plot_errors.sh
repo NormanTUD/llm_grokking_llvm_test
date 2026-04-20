a6dcd67a632cad873eb039635cbab587e2320efb
n <norman.koch@tu-dresden.de>
2026-04-20 07:32:08 +0200

    fix

---
+++ plot_errors.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# 1. Daten extrahieren und relative Werte berechnen
+data=$(cat epoch_*.txt | awk -F': ' '
+/expected/ {e=$2}
+/predicted/ {
+    p=$2;
+    if (e != 0 && e != "") {
+        print ((p - e) / (e < 0 ? -e : e)) * 100
+    }
+}')
+
+# 2. Median berechnen
+median=$(echo "$data" | sort -n | awk '
+{ a[NR]=$1 }
+END {
+    if (NR % 2) {
+        print a[(NR + 1) / 2]
+    } else {
+        print (a[(NR / 2)] + a[(NR / 2) + 1]) / 2
+    }
+}')
+
+echo "Berechneter Median: $median%"
+
+# 3. Filterung und Plotting mit Gnuplot
+# Filter: wert > (median - 50) && wert < (median + 50)
+echo "$data" | awk -v m="$median" '
+{ if ($1 > (m - 50) && $1 < (m + 50)) print $1 }
+' > filtered_data.dat
+
+gnuplot -p <<EOF
+set title "Relative Abweichung (Gefiltert: Median ±50%)"
+set ylabel "Abweichung in %"
+set xlabel "Datenpunkt"
+set grid
+plot "filtered_data.dat" with linespoints title "Rel. Error"
+EOF
