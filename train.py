98c26372ca4bb7da6fdf3c6089341cefdc0e8720
n <norman.koch@tu-dresden.de>
2026-04-23 23:15:59 +0200

    fix

--- train.py
+++ train.py
@@ -144,9 +144,9 @@ def parse_args() -> argparse.Namespace:
                    help="Max operations in random DAG")
     g.add_argument("--allowed-ops", type=str, default="add,sub",
                    help="Comma-separated LLVM ops")
-    g.add_argument("--param-min", type=int, default=-200,
+    g.add_argument("--param-min", type=int, default=-15,
                    help="Min random parameter value")
-    g.add_argument("--param-max", type=int, default=200,
+    g.add_argument("--param-max", type=int, default=15,
                    help="Max random parameter value")
 
     g = p.add_argument_group("Model Architecture")
